cf0902fa518228ffb777c9408cf417ec9b2018ac
giubadileao <giuliaolvrsantos@gmail.com>
2026-08-11 17:40:01 -0300

    Add event listener to play video on button click

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const botao = document.getElementById("botao");
+const telaInicial = document.querySelector(".tela-inicial");
+const videoContainer = document.getElementById("videoContainer");
+const video = document.getElementById("video");
 
+botao.addEventListener("click", function() {
+  telaInicial.style.display = "none";
+  videoContainer.style.display = "flex";
+  video.play();
+});
