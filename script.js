3dbe0b83f91bbdae6bdec9c3d0c5359ac1541efb
giubadileao <giuliaolvrsantos@gmail.com>
2026-08-11 21:50:38 -0300

    Notify button functionality with alert
    Added alert to indicate button functionality.

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 function tocarVideo() {
-document.querySelector(".tela-inicial").style.display = "none";
-document.getElementById("videoContainer").style.display = "flex";
-const video = document.getElementById("video");
-  video.play();
+alert("O botao funcionou!");
 }
